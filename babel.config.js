0b857f60d483e320703a1ae52ecf199b75473ebf
Emmanuele <emmanuele.online@gmail.com>
2021-09-09 13:51:16 +0200

    removed  " import 'roboto-fontface/css/roboto/roboto-fontface.css' import '@mdi/font/css/materialdesignicons.css'" from main.js

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
-// babel.config.js
 module.exports = {
   presets: [
-    ['@vue/app', {
-      polyfills: [
-        'es.promise',
-        'es.symbol'
-      ]
-    }]
+    '@vue/cli-plugin-babel/preset'
   ]
 }
