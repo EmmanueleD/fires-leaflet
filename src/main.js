f491302dc755b2cc0f257a894b5383d4d0a035fb
Emmanuele <emmanuele.online@gmail.com>
2021-09-09 12:34:27 +0200

    correct "core-js dependencies not found"

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import 'leaflet/dist/leaflet.css';
-
+import 'core-js'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
